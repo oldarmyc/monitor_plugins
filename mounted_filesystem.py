7c25bba01cb721a5d9d12e7259de348428f1d831
davi4261 <david.kludt@rackspace.com>
2016-01-27 13:56:09 -0600

    Add in mounted filesystem custom plugin check

---
+++ mounted_filesystem.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+
+import sys
+import os
+
+
+def check_file_system_read_write(file_system_path):
+    test_file_path = '{}/temp_test'.format(file_system_path)
+    try:
+        file = open(test_file_path, 'w')
+        file.close()
+        os.remove(test_file_path)
+    except:
+        return 'RO'
+    else:
+        return 'RW'
+
+
+def main():
+    status = 'RO'
+    if len(sys.argv) != 2:
+        print("Usage: {} <filePath>".format(sys.argv[0]))
+        sys.exit(0)
+
+    file_system_path = sys.argv[1]
+    status = check_file_system_read_write(file_system_path)
+    print("metric writestatus string {}".format(status))
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
