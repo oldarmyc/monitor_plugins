06228beb216d8c6d1cfb54035606240c5714d328
davi4261 <david.kludt@rackspace.com>
2016-01-27 14:30:28 -0600

    Update print statements to ensure it works with 2.6.6

--- mounted_filesystem.py
+++ mounted_filesystem.py
@@ -6,7 +6,7 @@ import os
 
 
 def check_file_system_read_write(file_system_path):
-    test_file_path = '{}/temp_test'.format(file_system_path)
+    test_file_path = '%s/temp_test' % (file_system_path)
     try:
         file = open(test_file_path, 'w')
         file.close()
@@ -20,12 +20,12 @@ def check_file_system_read_write(file_system_path):
 def main():
     status = 'RO'
     if len(sys.argv) != 2:
-        print("Usage: {} <filePath>".format(sys.argv[0]))
+        print('Usage: %s <filePath>' % (sys.argv[0]))
         sys.exit(0)
 
     file_system_path = sys.argv[1]
     status = check_file_system_read_write(file_system_path)
-    print("metric writestatus string {}".format(status))
+    print('metric writestatus string %s' % (status))
     sys.exit(0)
 
 
